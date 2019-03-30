e8bbb607a957593c648ab5fe1fccdb16fba51819
Simon Gibbons <simongibbons@gmail.com>
2019-03-30 17:03:29 +0000

    Fixes

--- handler.py
+++ handler.py
@@ -19,7 +19,6 @@ SPORTS = [
 
 
 def post_tweet(event, context):
-    return {'message': 'Hello'}
     client = get_twitter_client_from_environment()
     message = get_tweet_message(ANIMALS, SPORTS)
 
@@ -32,13 +31,7 @@ def post_tweet(event, context):
 def get_tweet_message(animals: List[str], sports: List[str]) -> str:
     animal = random.choice(animals)
     sport = random.choice(sports)
-    return f"Ain't no rule that says an {animal} can't play {sport}"
-
-
-def get_article(noun):
-    if len(noun) == 0:
-        return 'a'
-    pass
+    return f"Ain't no rule that says a {animal} can't play {sport}"
 
 
 def get_twitter_client_from_environment():
@@ -54,7 +47,3 @@ def get_twitter_client(consumer_key, consumer_secret, access_key, access_secret)
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_key, access_secret)
     return tweepy.API(auth)
-
-
-if __name__ == '__main__':
-    print("Yo!")
